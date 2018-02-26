40a9939d4117288ce1cdfbd7d8beb4c7103ae9e0
U-ben-PC\ben <benjprice99@gmail.com>
2018-02-26 15:51:01 +0000

    Developed main method

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,14 +3,28 @@ class BubbleSort {
     private int[] array;
 
     public void main (String[] args) {
-
+        //Create unsorted array
+        //Sort array
+        //Print sorted array
+        int[] unsorted = new int[]{5,3,9,2,6,1};
+        BubbleSort bubble = new BubbleSort();
+        bubble.setArray(unsorted);
+        bubble.sortArray();
+        int[] sorted = bubble.getArray();
+        for (int i=0; i<sorted.length; i++) {
+            System.out.println(sorted[i]);
+        }
     }
 
-    public void setArray() {
-
+    public void setArray(int[] a) {
+        array = a.clone();
     }
 
     public int[] getArray() {
+        return null;
+    }
+
+    public int[] sortArray() {
         return null;
     }
 
