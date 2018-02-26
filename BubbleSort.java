26cb0264cffdcb918114b1aa4b1b7769c4dd32c2
U-ben-PC\ben <benjprice99@gmail.com>
2018-02-26 15:52:27 +0000

    Made main static

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,7 +2,7 @@ class BubbleSort {
 
     private int[] array;
 
-    public void main (String[] args) {
+    public static void main (String[] args) {
         //Create unsorted array
         //Sort array
         //Print sorted array
